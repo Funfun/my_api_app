b4b1b9129fcbae15a787a82185a22c5bf4a1e590
Tsyren Ochirov <nsu1team@gmail.com>
2015-12-03 22:19:44 +0100

    Clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
   end
 
   def authenticate
-    if user = authenticate_with_http_basic { |login, password| puts(login+password); User.authenticate(login, password) }
+    if user = authenticate_with_http_basic { |login, password| User.authenticate(login, password) }
       @current_user = user
     else
       request_http_basic_authentication
